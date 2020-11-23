7078c2dd6d8a61aa27d8dc7e1d8db28e07a5afa3
Andre Bonetti De Oliveira <andre.oliveira@portaldedocumentos.com.br>
2020-11-23 17:53:26 -0300

    backend empresa ok

--- backend/src/main/java/br/com/controlechamados/Application.java
+++
@@ -1,2 +0,0 @@
-package main.java.br.com.controlechamados;public class Aplication {
-}
--- backend/src/main/java/br/com/controlechamados/occurrence/OccurrenceController.java
+++
@@ -1,4 +0,0 @@
-package main.java.br.com.controlechamados.occurrence;
-
-public class OccurrenceController {
-}
