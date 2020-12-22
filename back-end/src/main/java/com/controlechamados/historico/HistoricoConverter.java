7309c4085b72d6cda6718a2ffbaa1419c570aa37
André Bonetti <andrebonetti2@gmail.com>
2020-12-21 23:32:53 -0300

    Controller findAll e detalhe

--- back-end/src/main/java/com/controlechamados/historico/HistoricoConverter.java
+++ back-end/src/main/java/com/controlechamados/historico/HistoricoConverter.java
@@ -1,5 +1,27 @@
 package com.controlechamados.historico;
 
+import com.controlechamados.historico.dto.HistoricoGridDTO;
+import com.controlechamados.usuario.UsuarioConverter;
+import com.controlechamados.usuario.dto.UsuarioSimpleGridDTO;
+
 public class HistoricoConverter {
+
+    public static HistoricoGridDTO toGridDTO(Historico historico){
+
+        UsuarioSimpleGridDTO usuarioSimpleGridDTO =
+                UsuarioConverter.toSimpleGridDto(historico.getUsuarioAcao());
+
+        return HistoricoGridDTO.builder()
+                .withDados(historico.getDados())
+                .withIdRegistro(historico.getIdRegistro())
+                .withTipoAcao(historico.getTipoAcao().getValue())
+                .withVersion(historico.getVersion())
+                .withDataAcao(historico.getDataAcao().toString())
+                .withUsuarioAcaoGRID(usuarioSimpleGridDTO)
+                .withTabela(historico.getTabela().getValue())
+                .withId(historico.getId())
+                .build();
+
+    }
 
 }
