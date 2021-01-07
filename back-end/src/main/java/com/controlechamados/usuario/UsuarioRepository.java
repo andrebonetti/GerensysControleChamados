9cfd70810f8814b3cdf00e20a50ab474725e519c
André Bonetti <andrebonetti2@gmail.com>
2021-01-07 12:45:41 -0300

    gitflow-feature-stash: SB-12-tratar-mensagens-exception
    Alteração Banco

--- back-end/src/main/java/com/controlechamados/usuario/UsuarioRepository.java
+++ back-end/src/main/java/com/controlechamados/usuario/UsuarioRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
-    public Iterable<Usuario> findAll();
-
 }
